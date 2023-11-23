a4fe1148ff52b19fd5a12b9a1b27eff2ad6a2bf7
Sebastián Astudillo <sebastianastudillorojas@gmail.com>
2023-11-23 01:01:14 -0300

    API documentation

--- src/proyect/proyect.controller.ts
+++ src/proyect/proyect.controller.ts
@@ -3,52 +3,74 @@ import { ProyectService } from './proyect.service';
 import { CreateProyectDto } from './dto/create-proyect.dto';
 import { UpdateProyectDto } from './dto/update-proyect.dto';
 import { BodyTeam } from './entities/bodyAddTeam.entity';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Proyect } from '../schema/proyect.schema';
+import { Team } from 'src/team/entity/team.entity';
 
 
-@ApiTags('ProyectE')
+@ApiTags('Proyect')
 @ApiBearerAuth()
 @Controller('proyect')
 export class ProyectController {
   constructor(private readonly proyectService: ProyectService) {}
 
+  @ApiOperation({ summary: 'Crea un nuevo proyecto' })
+  @ApiBody({ type: CreateProyectDto })
+  @ApiResponse({ status: 201, type: Proyect })
   @Post()
   create(@Body() createProyectDto: CreateProyectDto, @Req() request: Request): Promise<Proyect> {
     const token = request.headers['authorization'].split(" ")[1];
     return this.proyectService.create(createProyectDto, token);
   }
 
+  @ApiOperation({ summary: 'Obtiene todos los proyectos del usuario' })
+  @ApiResponse({ status: 200, type: Proyect, isArray: true })
   @Get()
   findAll(@Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.proyectService.findAll(token);
   }
 
+  @ApiOperation({ summary: 'Actualiza un proyecto por su ID' })
+  @ApiParam({ name: 'id', required: true, description: 'ID del proyecto' })
+  @ApiBody({ type: UpdateProyectDto })
+  @ApiResponse({ status: 200, type: Proyect })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProyectDto: UpdateProyectDto, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.proyectService.update(id, updateProyectDto, token);
   }
 
+  @ApiOperation({ summary: 'Elimina un proyecto por su ID' })
+  @ApiParam({ name: 'id', required: true, description: 'ID del proyecto' })
+  @ApiResponse({ status: 200 })
   @Delete(':id')
   remove(@Param('id') id: string, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.proyectService.remove(id, token);
   }
 
+  @ApiOperation({ summary: 'Añade un equipo a un proyecto' })
+  @ApiBody({ type: BodyTeam })
+  @ApiResponse({ status: 200, type: Team })
   @Post('/addteam')
   addTeam(@Body() body: BodyTeam, @Req() request: Request){
     const token = request.headers['authorization'].split(" ")[1];
     return this.proyectService.addTeam(body, token);
   }
 
+  @ApiOperation({ summary: 'Elimina un equipo de un proyecto por su ID' })
+  @ApiParam({ name: 'idteam', required: true, description: 'ID del equipo' })
+  @ApiResponse({ status: 200 })
   @Delete('/deleteteam/:idteam')
   deleteTeam(@Param('idteam') idteam: string, @Req() request: Request){
     const token = request.headers['authorization'].split(" ")[1];
     return this.proyectService.deleteTeam(idteam, token);
   }
 
+  @ApiOperation({ summary: 'Obtiene todos los equipos de un proyecto por su ID' })
+  @ApiParam({ name: 'idproyect', required: true, description: 'ID del proyecto' })
+  @ApiResponse({ status: 200, type: Team, isArray: true })
   @Get('/teams/:idproyect')
   findTeamsProyect(@Param('idproyect') idproyect: string, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -2,8 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query } from '@
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TaskState } from './entities/TaskState.enum';
+import { Task } from './entities/task.entity';
 
 
 @ApiTags('Task')
@@ -11,7 +12,10 @@ import { TaskState } from './entities/TaskState.enum';
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
-
+  
+  @ApiOperation({ summary: 'Crea una nueva tarea' })
+  @ApiBody({ type: CreateTaskDto }) // Tipo de dato esperado en el body
+  @ApiResponse({ status: 201, type: Task }) // Tipo de dato retornado
   @Post()
   create(@Body() createTaskDto: CreateTaskDto, @Req() request: Request) {
     console.log(createTaskDto);
@@ -19,42 +23,67 @@ export class TaskController {
     return this.taskService.create(createTaskDto, token);
   }
 
+  @ApiOperation({ summary: 'Obtiene todas las tareas de un proyecto' })
+  @ApiQuery({ name: 'id_proyect', required: true, description: 'ID del proyecto' })
+  @ApiResponse({ status: 200, type: Task, isArray: true })
   @Get(':id_proyect')
   findAll(@Param('id_proyect') id_proyect: string, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.taskService.findAll(id_proyect, token);
   }
 
+
+  @ApiOperation({ summary: 'Obtiene una tarea por su ID' })
+  @ApiQuery({ name: 'id_task', required: true, description: 'ID de la tarea' })
+  @ApiResponse({ status: 200, type: Task })
   @Get(':id_task')
   findOne(@Param('id_task') id_task: string, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.taskService.findOne(id_task, token);
   }
 
+  @ApiOperation({ summary: 'Actualiza una tarea por su ID' })
+  @ApiQuery({ name: 'id_task', required: true, description: 'ID de la tarea' })
+  @ApiBody({ type: UpdateTaskDto }) // Tipo de dato esperado en el body
+  @ApiResponse({ status: 200, type: Task })
   @Patch(':id_task')
   update(@Param('id_task') id_task: string, @Body() updateTaskDto: UpdateTaskDto, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.taskService.update(id_task, updateTaskDto, token);
   }
 
+  @ApiOperation({ summary: 'Elimina una tarea por su ID' })
+  @ApiQuery({ name: 'id_task', required: true, description: 'ID de la tarea' })
+  @ApiResponse({ status: 200 })
   @Delete(':id_task')
   remove(@Param('id_task') id_task: string, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.taskService.remove(id_task, token);
   }
 
+  @ApiOperation({ summary: 'Inicializa una tarea por su ID' })
+  @ApiQuery({ name: 'id_task', required: true, description: 'ID de la tarea' })
+  @ApiResponse({ status: 200, type: Task })
   @Patch(':id_task')
   initTask(@Param('id_task') id_task: string, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.taskService.initTask(id_task, token);
   }
 
+  @ApiOperation({ summary: 'Finaliza una tarea por su ID' })
+  @ApiQuery({ name: 'id_task', required: true, description: 'ID de la tarea' })
+  @ApiResponse({ status: 200, type: Task })
   @Patch(':id_task')
   finishTask(@Param('id_task') id_task: string, @Req() request: Request) {
     const token = request.headers['authorization'].split(" ")[1];
     return this.taskService.finishTask(id_task, token);
   }
 
+  @ApiOperation({ summary: 'Busca tareas por varios parámetros' })
+  @ApiQuery({ name: 'name', required: false, description: 'Nombre de la tarea' })
+  @ApiQuery({ name: 'state', required: false, description: 'Estado de la tarea' })
+  @ApiQuery({ name: 'nameResponsible', required: false, description: 'Nombre del responsable de la tarea' })
+  @ApiResponse({ status: 200, type: Task, isArray: true })
   @Get('search')
   @ApiQuery({ name: 'name', required: false })
   @ApiQuery({ name: 'state', required: false })
