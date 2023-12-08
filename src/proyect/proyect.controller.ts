172d68975fc872d84127dbcf2ab1face0074201e
Sebastián Astudillo <sebastianastudillorojas@gmail.com>
2023-12-08 02:31:41 -0300

    add error monitoring on endpoints

--- src/proyect/proyect.controller.ts
+++ src/proyect/proyect.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, HttpException } from '@nestjs/common';
 import { ProyectService } from './proyect.service';
 import { CreateProyectDto } from './dto/create-proyect.dto';
 import { UpdateProyectDto } from './dto/update-proyect.dto';
@@ -7,6 +7,7 @@ import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } 
 import { Proyect } from '../schema/proyect.schema';
 import { Team } from 'src/team/entity/team.entity';
 import { UpdateTeamDto } from 'src/team/dto/update-team.dto';
+import { HttpStatusCode } from 'axios';
 
 
 @ApiTags('Proyect')
@@ -20,16 +21,26 @@ export class ProyectController {
   @ApiResponse({ status: 201, type: Proyect })
   @Post()
   create(@Body() createProyectDto: CreateProyectDto, @Req() request: Request): Promise<Proyect> {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.create(createProyectDto, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.create(createProyectDto, token);
+    }catch(error){
+      console.error('Error en create:', error);
+      throw new HttpException('No fue posible crear el proyecto', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Obtiene todos los proyectos del usuario' })
   @ApiResponse({ status: 200, type: Proyect, isArray: true })
   @Get()
   findAll(@Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.findAll(token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.findAll(token);
+    }catch(error){
+      console.error('Error en findAll:', error);
+      throw new HttpException('No fue posible obtener todos los proyecto del usuario', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Actualiza un proyecto por su ID' })
@@ -38,8 +49,13 @@ export class ProyectController {
   @ApiResponse({ status: 200, type: Proyect })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProyectDto: UpdateProyectDto, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.update(id, updateProyectDto, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.update(id, updateProyectDto, token);
+    }catch(error){
+      console.error('Error en update:', error);
+      throw new HttpException('No fue posible Actualizar un proyecto por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Elimina un proyecto por su ID' })
@@ -47,8 +63,13 @@ export class ProyectController {
   @ApiResponse({ status: 200 })
   @Delete(':id')
   remove(@Param('id') id: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.remove(id, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.remove(id, token);
+    }catch(error){
+      console.error('Error en remove:', error);
+      throw new HttpException('No fue posible Eliminar un proyecto por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Añade un equipo a un proyecto' })
@@ -56,8 +77,13 @@ export class ProyectController {
   @ApiResponse({ status: 200, type: Team })
   @Post('/addteam')
   addTeam(@Body() body: BodyTeam, @Req() request: Request){
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.addTeam(body, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.addTeam(body, token);
+    }catch(error){
+      console.error('Error en addTeam:', error);
+      throw new HttpException('No fue posible Añadir un equipo a un proyecto', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Elimina un equipo de un proyecto por su ID' })
@@ -65,8 +91,13 @@ export class ProyectController {
   @ApiResponse({ status: 200 })
   @Delete('/deleteteam/:idteam')
   deleteTeam(@Param('idteam') idteam: string, @Req() request: Request){
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.deleteTeam(idteam, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.deleteTeam(idteam, token);
+    }catch(error){
+      console.error('Error en deleteTeam:', error);
+      throw new HttpException('No fue posible Eliminar un equipo de un proyecto por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Obtiene todos los equipos de un proyecto por su ID' })
@@ -74,8 +105,13 @@ export class ProyectController {
   @ApiResponse({ status: 200, type: Team, isArray: true })
   @Get('/teams/:idproyect')
   findTeamsProyect(@Param('idproyect') idproyect: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.findTeamProyect(idproyect, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.findTeamProyect(idproyect, token);
+    }catch(error){
+      console.error('Error en findTeamsProyect:', error);
+      throw new HttpException('No fue posible Obtener todos los equipos de un proyecto por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Elimina todos los equipos con el nombre especificado del sistema' })
@@ -83,8 +119,13 @@ export class ProyectController {
   @ApiResponse({ status: 200 })
   @Post('/teams/:nameteam')
   deleteTeamMay(@Param('nameteam') nameteam: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.deleteTeamMany(nameteam,token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.deleteTeamMany(nameteam,token);
+    }catch(error){
+      console.error('Error en deleteTeamMay:', error);
+      throw new HttpException('No fue posible Eliminar todos los equipos con el nombre especificado del sistema', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Actualiza el nombre de un equipo' })
@@ -93,7 +134,12 @@ export class ProyectController {
   @ApiResponse({ status: 200, type: Team })
   @Patch('team/:nameteam')
   updateTeam(@Param('nameteam') nameteam: string, @Body() updateTeam: UpdateTeamDto, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.proyectService.updateTeamMany(nameteam,token, updateTeam);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.proyectService.updateTeamMany(nameteam,token, updateTeam);
+    }catch(error){
+      console.error('Error en updateTeam:', error);
+      throw new HttpException('No fue posible Actualizar el nombre de un equipo', HttpStatusCode.BadRequest);
+    }
   }
 }
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query, HttpException } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Task } from './entities/task.entity';
+import { HttpStatusCode } from 'axios';
 
 
 @ApiTags('Task')
@@ -17,8 +18,14 @@ export class TaskController {
   @ApiResponse({ status: 201, type: Task }) // Tipo de dato retornado
   @Post()
   create(@Body() createTaskDto: CreateTaskDto, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.create(createTaskDto, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.taskService.create(createTaskDto, token);
+    }catch (error){
+      console.error('Error en create:', error);
+      throw new HttpException('No fue posible Crear una nueva tarea', HttpStatusCode.BadRequest);
+    }
+    
   }
 
   @ApiOperation({ summary: 'Obtiene todas las tareas de un proyecto' })
@@ -26,8 +33,13 @@ export class TaskController {
   @ApiResponse({ status: 200, type: Task, isArray: true })
   @Get(':id_proyect')
   findAll(@Param('id_proyect') id_proyect: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.findAll(id_proyect, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.taskService.findAll(id_proyect, token);
+    }catch (error){
+      console.error('Error en findAll:', error);
+      throw new HttpException('No fue posible Obtener todas las tareas de un proyecto', HttpStatusCode.BadRequest);
+    }
   }
 
 
@@ -36,8 +48,13 @@ export class TaskController {
   @ApiResponse({ status: 200, type: Task })
   @Get(':id_task')
   findOne(@Param('id_task') id_task: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.findOne(id_task, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.taskService.findOne(id_task, token);
+    }catch (error){
+      console.error('Error en findOne:', error);
+      throw new HttpException('No fue posible Obtener una tarea por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Actualiza una tarea por su ID' })
@@ -46,8 +63,14 @@ export class TaskController {
   @ApiResponse({ status: 200, type: Task })
   @Patch(':id_task')
   update(@Param('id_task') id_task: string, @Body() updateTaskDto: UpdateTaskDto, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.update(id_task, updateTaskDto, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.taskService.update(id_task, updateTaskDto, token);
+    }catch (error){
+      console.error('Error en update:', error);
+      throw new HttpException('No fue posible Actualizar una tarea por su ID', HttpStatusCode.BadRequest);
+    }
+    
   }
 
   @ApiOperation({ summary: 'Elimina una tarea por su ID' })
@@ -55,8 +78,13 @@ export class TaskController {
   @ApiResponse({ status: 200 })
   @Delete(':id_task')
   remove(@Param('id_task') id_task: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.remove(id_task, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.taskService.remove(id_task, token);
+    }catch (error){
+      console.error('Error en remove:', error);
+      throw new HttpException('No fue posible Eliminar una tarea por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Inicializa una tarea por su ID' })
@@ -64,8 +92,13 @@ export class TaskController {
   @ApiResponse({ status: 200, type: Task })
   @Patch('init/:id_task')
   initTask(@Param('id_task') id_task: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.initTask(id_task, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.taskService.initTask(id_task, token);
+    }catch (error){
+      console.error('Error en initTask:', error);
+      throw new HttpException('No fue posible Inicializar una tarea por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
   @ApiOperation({ summary: 'Finaliza una tarea por su ID' })
@@ -73,21 +106,13 @@ export class TaskController {
   @ApiResponse({ status: 200, type: Task })
   @Patch('finish/:id_task')
   finishTask(@Param('id_task') id_task: string, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.finishTask(id_task, token);
+    try{
+      const token = request.headers['authorization'].split(" ")[1];
+      return this.taskService.finishTask(id_task, token);
+    }catch (error){
+      console.error('Error en finishTask:', error);
+      throw new HttpException('No fue posible Finalizar una tarea por su ID', HttpStatusCode.BadRequest);
+    }
   }
 
-  @ApiOperation({ summary: 'Busca tareas por varios parámetros' })
-  @ApiQuery({ name: 'name', required: false, description: 'Nombre de la tarea' })
-  @ApiQuery({ name: 'state', required: false, description: 'Estado de la tarea' })
-  @ApiQuery({ name: 'nameResponsible', required: false, description: 'Nombre del responsable de la tarea' })
-  @ApiResponse({ status: 200, type: Task, isArray: true })
-  @Get('search')
-  @ApiQuery({ name: 'name', required: false })
-  @ApiQuery({ name: 'state', required: false })
-  @ApiQuery({ name: 'nameResponsible', required: false })
-  searchTasks(@Query() query: Record<string, any>, @Req() request: Request) {
-    const token = request.headers['authorization'].split(" ")[1];
-    return this.taskService.searchTasks(query, token);
-  }
 }
